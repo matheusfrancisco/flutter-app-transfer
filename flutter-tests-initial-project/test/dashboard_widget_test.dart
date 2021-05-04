6a9ff535099475412af67a95411378ae424c5ace
matheusfrancisco <matheusmachadoufsc@gmail.com>
2021-05-04 10:07:26 -0300

    + study test with flutter

---
+++ flutter-tests-initial-project/test/dashboard_widget_test.dart
@@ -0,0 +1,47 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/screens/dashboard.dart';
+
+void main() {
+  testWidgets('Should display the main image when the Dashboard is opened',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(MaterialApp(home:Dashboard()));
+        final mainImage = find.byType(Image);
+        expect(mainImage, findsOneWidget);
+      });
+
+  testWidgets('Should display the transfer feature when the Dashboard is opened',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(MaterialApp(home:Dashboard()));
+        //could be this way or using a predicate
+        // final iconTransferFeatureItem = find.widgetWithIcon(
+        //     FeatureItem,
+        //     Icons.monetization_on);
+        // expect(iconTransferFeatureItem, findsWidgets);
+        // final nameTransferFeatureItem = find.widgetWithText(FeatureItem, "Transfer");
+        // expect(nameTransferFeatureItem, findsOneWidget);
+            final transferFeatureItem = find.byWidgetPredicate((widget) =>
+                featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+            expect(transferFeatureItem, findsOneWidget);
+      });
+
+  testWidgets('Should display the transfer feature when the Dashboard is opened',
+          (WidgetTester tester) async {
+            await tester.pumpWidget(MaterialApp(home: Dashboard()));
+            final iconTransactionFeedFeatureItem = find.byWidgetPredicate((
+                widget) =>
+                featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+            expect(iconTransactionFeedFeatureItem, findsOneWidget);
+
+          });
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if(widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
+}
